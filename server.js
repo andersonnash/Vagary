0efda0bf07fbaf117dfe6bb6d45aa14be6c77446
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-08-02 15:43:53 -0400

    connected to server

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json);
+
+// app.use(cors());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("<h1>Vagary baby</h1>"));
+
+app.listen(PORT, console.log("We are connected to 3000!"));
