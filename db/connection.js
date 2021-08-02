8159871a074864c610705d1c25041ac752bf40d8
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-08-02 16:27:46 -0400

    Fixed express issue

--- db/connection.js
+++ db/connection.js
@@ -4,3 +4,5 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/vagary
 
 mongoose.set("debug", true);
 mongoose.set("useCreateIndex", true);
+
+export default mongoose.connection;
