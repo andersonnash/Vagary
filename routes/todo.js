d9562b839aa2ce1c5f4e9a1627b6deebede67cf9
luzmlx <luzlopez0820@gmail.com>
2021-08-02 15:44:42 -0400

    routes

---
+++ routes/todo.js
@@ -0,0 +1,20 @@
+import {Router} from "express"
+import restrict from "../helpers/restrict.js"
+import {getAllTodos, createTodo, getTodo, updateTodo, deleteTodo} from "../controllers/todos.js"
+
+// SUBJECT TO CHANGE: imported functions depending on whats being called in controllers
+// Make necessary changes once pulled.
+
+const router = Router();
+// get all todos
+router.get("/todos", restrict, getAllTodos)
+// get todo id
+router.get("todos/:id", getTodo)
+// post created todo
+router.post("/todos", restrict, createTodo)
+// update todo (PUT)
+router.put("/todos/:id", restrict, updateTodo)
+// delete Todo
+router.delete("/todos/:id", restrict, deleteTodo)
+
+export default router
