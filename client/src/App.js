c46bce5c4dbd91d52d8cd1d69432b7fc5beeca0e
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-08-03 13:05:32 -0600

    sign in form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-// import { Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Navbar from "./views/navbar/NavBar";
+import SignIn from "./views/signIn/SignIn";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Route path="/sign-in">
+        <SignIn />
+      </Route>
     </div>
   );
 }
