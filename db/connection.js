eb6eb790c96c24246226c0c2516d714a1f6980d6
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-08-03 11:33:26 -0400

    don't merge

--- db/connection.js
+++ db/connection.js
@@ -4,5 +4,17 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/vagary
 
 mongoose.set("debug", true);
 mongoose.set("useCreateIndex", true);
+mongoose.set("returnOriginal", true)
 
-export default mongoose.connection;
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+  .catch((error) =>
+    console.error("Error connecting to MongoDB:", error.message)
+);
+  
+mongoose.connection.on("error", (error) => console.error(`MongoDB connection error: ${error.message}`)
+);
+
+mongoose.connection.on("disconnected", () => console)
--- server.js
+++ server.js
@@ -14,3 +14,4 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => { console.log(req); res.send("<h1>Vagary baby</h1>")});
 
 app.listen(PORT, console.log(`Connected to port ${PORT}`));
+ 
