795210ef277d2a18ae5719490049217ab6df3f85
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-08-03 08:19:03 -0600

    changes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+// import { Router } from "express";
+
+// import db from "./db/connections.js";
+
+// import routes from "./routes/index";
 
 const app = express();
 const PORT = process.env.PORT || 4555;
@@ -11,6 +16,16 @@ app.use(cors());
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => { console.log(req); res.send("<h1>Vagary baby</h1>")});
+// app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("<h1>Vagary baby</h1>");
+});
 
 app.listen(PORT, console.log("We are connected to 4555!"));
+
+// db.on("connected", () => {
+//   console.log("Connected to MongoDB");
+//   app.listen(PORT, console.log(`connected to port: ${PORT}`));
+// });
