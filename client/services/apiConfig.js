5e0c5771add6f7b607053c4fa590e30600ff8bce
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-08-03 10:19:49 -0600

    index.js

--- client/services/apiConfig.js
+++ client/services/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4555/",
+  baseURL: "http://localhost:4567/api",
 });
 
 export default api;
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+// import { Route } from "react-router-dom";
+import Navbar from "./views/navbar/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
