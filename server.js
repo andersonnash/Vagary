f3038d4afe3f55e4e3f9f4cda8433d0b44a988c5
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-08-03 11:07:29 -0400

    Updated app.listen with string interpolation

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => { console.log(req); res.send("<h1>Vagary baby</h1>")});
 
-app.listen(PORT, console.log("We are connected to 4555!"));
+app.listen(PORT, console.log(`Connected to port ${PORT}`));
