d98d13dfaaa2c543f9e1837e1ec481198c027ec1
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-08-03 16:21:23 -0400

    importing navbar to homepage

---
+++ client/views/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import NavBar from "../navbar/NavBar.jsx";
+
+
+export default function Home(Props) {
+  return (
+    <NavBar />
+    <div></div>
+    <Footer />
+  )
+}
