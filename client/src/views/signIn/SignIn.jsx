230273321341df2b7eac3a7827b57d37e69d67b1
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-08-04 10:46:29 -0600

    sign in

--- client/src/views/signIn/SignIn.jsx
+++ client/src/views/signIn/SignIn.jsx
@@ -1,58 +1,6 @@
-import {useState} from "react";
-import {signIn} from "../../services/users"
-import { useHistory } from "react-router-dom";
-
-
-export default function SignIn(props) {
-    const [input, setInput] = useState({email: "", username: "", password: ""})
-    const { setUser} = props
-    const history = useHistory();
-
-const handleSubmit = async(e) => {
-    e.preventDefault()
-    const user = await SignIn(input)
-    setUser(user)
-    history.push("/");
-}
-
-const handleInput = (e) => {
-    const { id, value} = e.target
-    setInput((prevInput) => ({
-        ...prevInput,
-        [id]: value
-    }))
-}
-
-    return (
-      <div>
-        Sign In
-        <form className="signin-form" onSubmit={handleSubmit}>
-            <label>Email</label>
-            <input 
-            id="email"
-            type="email"
-            value={input.email}
-            onChange={handleInput}/>
-            <br />
-
-            <label>Password</label>
-            <input 
-            id="password"
-            type="password"
-            value={input.password}
-            onChange={handleInput}
-            />
-            <br />
-
-            <button>Sign In</button>
-        </form>
-      </div>  
-    )
-}
 import { useState } from "react";
-// import { signIn } from "../../services/user";
+import { signIn } from "../../services/user";
 import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 export default function SignIn(props) {
   const [input, setInput] = useState({ email: "", username: "", password: "" });
@@ -95,9 +43,8 @@ export default function SignIn(props) {
           onChange={handleInput}
         />
         <br />
-{/* Once button is clicked it goes to UserPage */}
-        <button >
-            <Link to="userpage">Sign In</Link></button>
+
+        <button>Sign In</button>
       </form>
     </div>
   );
