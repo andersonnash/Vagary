a79340a015222d586b5c07c5544a4cfcd15a1f3f
luzmlx <luzlopez0820@gmail.com>
2021-08-04 14:27:40 -0400

    signin css

--- client/src/views/signIn/SignIn.jsx
+++ client/src/views/signIn/SignIn.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
-import {signIn} from "../../services/users"
+// import {signIn} from "../../services/users"
 import { useHistory } from "react-router-dom";
+import "./SignIn.css"
 
 
 export default function SignIn(props) {
@@ -25,9 +26,10 @@ const handleInput = (e) => {
 
     return (
       <div>
-        Sign In
+        
         <form className="signin-form" onSubmit={handleSubmit}>
-            <label>Email</label>
+            <h3> Sign In</h3>
+            <label className="emaillabel">Email</label>
             <input 
             id="email"
             type="email"
@@ -35,7 +37,7 @@ const handleInput = (e) => {
             onChange={handleInput}/>
             <br />
 
-            <label>Password</label>
+            <label className="passwordlabel">Password</label>
             <input 
             id="password"
             type="password"
@@ -44,64 +46,9 @@ const handleInput = (e) => {
             />
             <br />
 
-            <button>Sign In</button>
+            <button className="signin-button">Sign In</button>
         </form>
       </div>  
     )
 }
-import { useState } from "react";
-// import { signIn } from "../../services/user";
-import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
-import "./SignIn.css"
 
-export default function SignIn(props) {
-  const [input, setInput] = useState({ email: "", username: "", password: "" });
-  const { setUser } = props;
-  const history = useHistory();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const user = await SignIn(input);
-    setUser(user);
-    history.push("/");
-  };
-
-  const handleInput = (e) => {
-    const { id, value } = e.target;
-    setInput((prevInput) => ({
-      ...prevInput,
-      [id]: value,
-    }));
-  };
-
-  return (
-    <div>
-      <h3>Sign in</h3>
-      <form className="signin-form" onSubmit={handleSubmit}>
-        <label className="email-label">Email</label>
-        <input
-          id="email"
-          type="email"
-          placeholder="Email"
-          value={input.email}
-          onChange={handleInput}
-        />
-        <br />
-
-        <label className="password-label">Password</label>
-        <input
-          id="password"
-          type="password"
-          placeholder="Password"
-          value={input.password}
-          onChange={handleInput}
-        />
-        <br />
-{/* Once button is clicked it goes to UserPage */}
-        <button className = "signin-button">
-            <Link to="userpage">Sign In</Link></button>
-      </form>
-    </div>
-  );
-}
